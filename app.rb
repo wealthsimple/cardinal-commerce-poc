bdc8931abf2382c636906c3e79e37927d3c52c55
Peter Graham <peterghm@gmail.com>
2020-12-30 18:39:08 -0500

    support another type of jwt

--- app.rb
+++ app.rb
@@ -10,10 +10,10 @@ require './utils/cardinal_jwt_helper'
 get '/' do
   cardinal_jwt_helper = CardinalJwtHelper.new(
     ws_reference_id: "ws_transaction_#{SecureRandom.uuid}",
-    confirm_url: "http://localhost:4567/3ds-callback-todo",
+    callback_url: "http://localhost:4567/3ds-callback-todo",
   )
 
   erb :index, locals: {
-    authentication_jwt: cardinal_jwt_helper.authentication_jwt,
+    cardinal_jwt: cardinal_jwt_helper.device_fingerprint_savebrowser_jwt,
   }
 end
--- utils/cardinal_jwt_helper.rb
+++ utils/cardinal_jwt_helper.rb
@@ -2,35 +2,47 @@ class CardinalJwtHelper
   # https://cardinaldocs.atlassian.net/wiki/spaces/CC/pages/196850/JWT+Creation
   # The JWT is a JWS with the signature using a SHA-256 HMAC hash algorithm. The JWT must be created server-side and sent to the front end to be injected into the JavaScript initialization code. Creating a JWT client-side is not a valid activation option. Each order should have a uniquely generated JWT associated with it.
 
-  def initialize(ws_reference_id:, confirm_url:)
+  def initialize(ws_reference_id:, callback_url:)
     @ws_reference_id = ws_reference_id
-    @confirm_url = confirm_url
+    @callback_url = callback_url
   end
 
   def authentication_jwt
-    encode_jwt(generate_jwt_payload(
-      ws_reference_id: @ws_reference_id,
-    ))
+    encode_jwt(generate_jwt_payload)
   end
 
   def transactional_jwt
     encode_jwt(generate_jwt_payload(
-      ws_reference_id: @ws_reference_id,
-      inner_payload: {
+      Payload: {
         OrderDetails: {
           OrderNumber: SecureRandom.uuid,
           Amount: 1500,
           CurrencyCode: '840',
         },
       },
-      confirm_url: @confirm_url,
+      ObjectifyPayload: true,
+      ConfirmUrl: @callback_url,
+    ))
+  end
+
+  def device_fingerprint_savebrowser_jwt
+    encode_jwt(generate_jwt_payload(
+      ReturnUrl: @callback_url,
+      ObjectifyPayload: true,
+      Payload: {
+        Consumer: {
+          Account: {
+            AccountNumber: "123456",
+          }
+        },
+      },
     ))
   end
 
   private
 
-  def generate_jwt_payload(ws_reference_id:, inner_payload: nil, confirm_url: nil)
-    payload = {
+  def generate_jwt_payload(additional_opts = {})
+    {
       # JWT Id - A unique identifier for this JWT. This field should change each time a JWT is generated.
       jti: SecureRandom.uuid,
       # Issued At - The epoch time in seconds of when the JWT was generated. This allows us to determine how long a JWT has been around and whether we consider it expired or not.
@@ -42,21 +54,8 @@ class CardinalJwtHelper
       # The merchant SSO OrgUnitId
       OrgUnitId: ENV.fetch('ORG_UNIT_ID'),
       # This is a merchant supplied identifier that can be used to match up data collected from Cardinal Cruise and Centinel. Centinel can then use data collected to enable rules or enhance the authentication request.
-      ReferenceId: ws_reference_id,
-    }
-    if inner_payload.present?
-      payload.merge!(
-        # The JSON data object being sent to Cardinal. This object is usually an Order object
-        Payload: inner_payload,
-        # A boolean flag that indicates how Centinel Api should consume the Payload claim. When set to true, this tells Centinel Api the Payload claim is an object. When set to false, the Payload claim is a stringified object. Some Jwt libraries do not support passing objects as claims, this allows those who only allow strings to use their libraries without customization
-        ObjectifyPayload: true,
-      )
-    end
-    if confirm_url.present?
-      # The merchant endpoint that will receive the post back from the payment brand that contains the Centinel API response JWT describing the result of redirecting to the payment brand.
-      payload.merge!(confirm_url: confirm_url)
-    end
-    payload
+      ReferenceId: @ws_reference_id,
+    }.merge(additional_opts)
   end
 
   def encode_jwt(payload)
