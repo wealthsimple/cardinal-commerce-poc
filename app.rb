28894c879b8967e0cbf03a00b5518a149ad872f1
Peter Graham <peterghm@gmail.com>
2021-01-04 18:01:25 -0500

    try out option3 with Simon

--- app.rb
+++ app.rb
@@ -7,13 +7,37 @@ require 'sinatra'
 
 require './utils/cardinal_jwt_helper'
 
+# Test card numbers for Cardinal from:
+# https://cardinaldocs.atlassian.net/wiki/spaces/CCen/pages/903577725/EMV+3DS+Test+Cases
+CARD_NUMBERS = {
+  VISA_SUCCESS: '4000000000001000',
+  VISA_FAIL: '4000000000001018',
+  MASTERCARD_SUCCESS: '5200000000001005',
+  MASTERCARD_FAIL: '5200000000001013',
+}
+
+# https://cardinaldocs.atlassian.net/wiki/spaces/CC/pages/360668/Cardinal+Cruise+Hybrid
 get '/' do
   cardinal_jwt_helper = CardinalJwtHelper.new(
     ws_reference_id: "ws_transaction_#{SecureRandom.uuid}",
     callback_url: "http://localhost:4567/3ds-callback-todo",
   )
 
-  erb :index, locals: {
-    cardinal_jwt: cardinal_jwt_helper.device_fingerprint_savebrowser_jwt,
+  erb :index_hybrid, locals: {
+    cardinal_jwt: cardinal_jwt_helper.transactional_jwt,
+    card_number: CARD_NUMBERS.fetch(:VISA_SUCCESS),
+  }
+end
+
+# https://cardinaldocs.atlassian.net/wiki/spaces/CC/pages/1109065757/Option+3+-+JWT+-+Card+BIN+as+a+POST+parameter+plus+JWT
+get '/option3' do
+  cardinal_jwt_helper = CardinalJwtHelper.new(
+    ws_reference_id: "ws_transaction_#{SecureRandom.uuid}",
+    callback_url: "http://localhost:4567/3ds-callback-todo",
+  )
+
+  erb :index_option3, locals: {
+    cardinal_jwt: cardinal_jwt_helper.transactional_jwt,
+    card_number: CARD_NUMBERS.fetch(:VISA_SUCCESS),
   }
 end
