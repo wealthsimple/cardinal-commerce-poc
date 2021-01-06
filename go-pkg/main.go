f9dc3c58e1f313a4f4abc30d9b914004045f5782
Peter Graham <peterghm@gmail.com>
2021-01-06 13:24:37 -0500

    remove duplicates

--- go-pkg/main.go
+++ go-pkg/main.go
@@ -29,9 +29,12 @@ func main() {
 		OrgUnit:          os.Getenv("ORG_UNIT"),
 		RequestSignature: signature,
 		Timestamp:        timestamp,
-		CardNumber:       "4000000000001091",
-		CardExpiryMonth:  "02",
-		CardExpiryYear:   "2024",
+		// Wealthsimple will provide TabaPay an AccountId, from which TabaPay can
+		// extract the below card details.
+		// Cardinal Sandbox card PANs can be found at https://cardinaldocs.atlassian.net/wiki/spaces/CCen/pages/903577725/EMV+3DS+Test+Cases
+		CardNumber:      "4000000000001091",
+		CardExpiryMonth: "02",
+		CardExpiryYear:  "2024",
 	}
 	requestBody, err := cmpilookup.GenerateCmpiRequestBodyXml(params)
 	if err != nil {
