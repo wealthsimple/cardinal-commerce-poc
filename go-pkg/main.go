310155a42052cb205a6e9b37b8471e710800b4cc
Peter Graham <peterghm@gmail.com>
2021-01-06 12:46:22 -0500

    format

--- go-pkg/main.go
+++ go-pkg/main.go
@@ -9,18 +9,18 @@ import (
 	"os"
 )
 
-func main () {
+func main() {
 	fmt.Println("Running cmpi_lookup request...")
 
 	timestamp := cmpilookup.GetRequestTimestampWithBuffer()
-	fmt.Println(timestamp)
+	fmt.Printf("Generated request timestamp: %q\n", timestamp)
 
 	signature, err := cmpilookup.GenerateCmpiRequestSignature(os.Getenv("API_KEY"), timestamp)
 
-	if (err != nil) {
+	if err != nil {
 		fmt.Printf("Error generating request signature: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("Generated request signature: %q\n", signature);
+	fmt.Printf("Generated request signature: %q\n", signature)
 }
