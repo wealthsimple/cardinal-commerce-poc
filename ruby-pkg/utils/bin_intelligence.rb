35ff3f34162ebbfdf22a53ea48faf96d662de8fc
Peter Graham <peterghm@gmail.com>
2021-02-23 16:03:31 -0500

    remove non-functional v2 endpoint
    Co-Authored-By: Jeremy Seto <50921000+JerSeto@users.noreply.github.com>

--- ruby-pkg/utils/bin_intelligence.rb
+++ ruby-pkg/utils/bin_intelligence.rb
@@ -16,16 +16,6 @@ class BinIntelligence
     @timestamp = (Time.now.utc - timestamp_buffer)
   end
 
-  # DOES NOT WORK ON STAGING. Deprecated?
-  def v2_perform_request
-    pp "Request body:", v2_request_body
-    RestClient.post(
-      ENV.fetch('BIN_INTELLIGENCE_V2_URL'),
-      v2_request_body.to_json,
-      { content_type: :json, accept: :json },
-    )
-  end
-
   # Response to this should look like the following:
   # {
   #   "TransactionId": "wsorder...",
@@ -49,33 +39,6 @@ class BinIntelligence
     Base64.strict_encode64(
       Digest::SHA256.digest("#{(@timestamp.to_f * 1000).to_i}#{@order_number}#{api_key}")
     ).strip
-  end
-
-  # DOES NOT WORK ON STAGING. Deprecated?
-  def v2_request_body
-    # {
-    #   "Signature": "rDblGQSJujgHEeuvqTbJjB6Fktsodddiri6+F5do9cA=",
-    #   "Timestamp": "2018-08-12T14:23:02.941Z",
-    #   "Identifier": "aalkjdfalkdjfaslkdj",
-    #   "Algorithm": "SHA-256",
-    #   "TransactionId": "132456789",
-    #   "OrgUnitId": "565607c18b111e058463ds8r",
-    #   "Payload": {
-    #     "BINs": "44444444"
-    #   }
-    # }
-    {
-      Signature: request_signature,
-      Timestamp: @timestamp.iso8601(3),
-      Identifier: ENV.fetch('API_IDENTIFIER'),
-      Algorithm: SIGNATURE_ALGORITHM,
-      # Alpha numeric value transactionId. Length 5-55 characters long
-      TransactionId: @order_number,
-      OrgUnitId: ENV.fetch('ORG_UNIT_ID'),
-      Payload: {
-        BINs: @card_number,
-      },
-    }
   end
 
   def v3_request_body
